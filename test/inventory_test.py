6633dfe0b947a79856123c391726dd42c3bad737
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-08 15:40:46 -0400

    Starter versions of these tests

--- test/inventory_test.py
+++ test/inventory_test.py
@@ -1,2 +1,33 @@
+import csv
+import os
+
+#from app.inventory_app import read_from_csv, write_to_csv, autogen_id
+
+# TODO: test logic for processing CSV file contents into a list of product dictionaries
+def test_read_from_csv():
+    default_products_filepath = os.path.join(os.path.dirname(__file__), "..", "test", "mock_data", "default_products.csv")
+
+    default_products = [] # read_from_csv(default_products_filepath)
+
+    assert len(default_products) == 20
+
+
+# TODO: test logic for writing a list of product dictionaries to a CSV file
+def test_write_to_csv():
+    temp_filepath = os.path.join(os.path.dirname(__file__), "..", "test", "mock_data", "temp_products.csv")
+
+    if os.path.isfile(temp_filepath):
+        os.remove(temp_filepath)
+
+    assert os.path.isfile(temp_filepath) == False
+
+    products = []
+
+    #write_to_file(products, temp_filepath)
+
+    assert os.path.isfile(temp_filepath) == True
+
 
 # TODO: test logic for auto-incrementing the identifier of a product about to be created
+def test_autogen_id():
+    assert False
