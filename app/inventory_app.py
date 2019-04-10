22f79a813d1a23f8ccab23fe2b423c75fc4fc682
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-10 17:40:01 -0400

    Prepare to test

--- app/inventory_app.py
+++ app/inventory_app.py
@@ -4,56 +4,61 @@ import os
 csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
 csv_headers = ["id", "name", "aisle", "department", "price"]
 
-#
-# READ
-#
+def read_from_csv(my_csv_filepath):
+    return []
 
-products = []
+if __name__ == "__main__":
 
-with open(csv_filepath, "r") as csv_file:
-    reader = csv.DictReader(csv_file)
-    for ordered_dict in reader:
-        products.append(dict(ordered_dict))
+    #
+    # READ
+    #
 
-#
-# LIST
-#
+    products = []
 
-print("---------------------")
-print("LISTING PRODUCTS:")
+    with open(csv_filepath, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for ordered_dict in reader:
+            products.append(dict(ordered_dict))
 
-for product in products:
-    print(f"  {product['id']}) {product['name']} ")
+    #
+    # LIST
+    #
 
-#
-# CREATE
-#
-
-print("---------------------")
-print("CREATING A NEW PRODUCT:")
-
-new_product = {
-    "name": "My New Product",
-    "aisle": "Some Aisle",
-    "department": "Some Department",
-    "price": 99.99
-} # further exploration / todo: ask for user to input
-
-next_id = len(products) + 1
-
-new_product["id"] = next_id
-
-print(new_product)
-
-products.append(new_product)
-
-#
-# WRITE
-#
-
-with open(csv_filepath, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-    writer.writeheader()
+    print("---------------------")
+    print("LISTING PRODUCTS:")
 
     for product in products:
-        writer.writerow(product)
+        print(f"  {product['id']}) {product['name']} ")
+
+    #
+    # CREATE
+    #
+
+    print("---------------------")
+    print("CREATING A NEW PRODUCT:")
+
+    new_product = {
+        "name": "My New Product",
+        "aisle": "Some Aisle",
+        "department": "Some Department",
+        "price": 99.99
+    } # further exploration / todo: ask for user to input
+
+    next_id = len(products) + 1
+
+    new_product["id"] = next_id
+
+    print(new_product)
+
+    products.append(new_product)
+
+    #
+    # WRITE
+    #
+
+    with open(csv_filepath, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader()
+
+        for product in products:
+            writer.writerow(product)
